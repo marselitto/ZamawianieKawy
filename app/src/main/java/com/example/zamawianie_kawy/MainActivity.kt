f7900a6298e226f6028b54802e225203e28bb522
jj <twojemail@example.com>
2025-03-12 19:11:33 +0100

    Update app

--- app/src/main/java/com/example/zamawianie_kawy/MainActivity.kt
+++ app/src/main/java/com/example/zamawianie_kawy/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.zamawianie_kawy
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.RadioButton
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,11 +22,40 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val kawaespresso = findViewById<RadioButton>(R.id.espresso)
+        val kawacapuccino = findViewById<RadioButton>(R.id.capuccino)
+        val kawalatte = findViewById<RadioButton>(R.id.latte)
+        val azdjecie = findViewById<ImageView>(R.id.zdjecie)
         kawaespresso.setOnCheckedChangeListener {_, isChecked ->
             if(isChecked){
-
+                azdjecie.setImageResource(R.drawable.espresso)
             }
         }
+
+        kawacapuccino.setOnCheckedChangeListener {_, isChecked ->
+            if(isChecked){
+                azdjecie.setImageResource(R.drawable.capuccino)
+            }
+        }
+
+        kawalatte.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked){
+                azdjecie.setImageResource(R.drawable.latte)
+            }
+        }
+
+        val seekBar = findViewById<SeekBar>(R.id.ilosc)
+        val ilosckaw = findViewById<TextView>(R.id.ilosckaw)
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                ilosckaw.text = "Ile kaw chcesz zamówić: $progress"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+            }
+        })
     }
 
 
